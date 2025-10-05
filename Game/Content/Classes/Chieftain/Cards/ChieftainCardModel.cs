8072e7f897938e1d55e176a1304b706c652215cc
Sergey Burgsdorf <sergey.burgsdorf@gmail.com>
2025-10-05 12:13:31 +0200

    Chieftain assets and mount trait

--- Game/Content/Classes/Chieftain/Cards/ChieftainCardModel.cs
+++ Game/Content/Classes/Chieftain/Cards/ChieftainCardModel.cs
@@ -2,9 +2,9 @@
 	where TTop : ChieftainCardSide, new()
 	where TBottom : ChieftainCardSide, new()
 {
-	//protected override string TexturePath => "res://Content/Classes/Chieftain/Cards.jpg";
-	//protected override int ColumnCount => 6;
-	//protected override int RowCount => 5;
+	protected override string TexturePath => "res://Content/Classes/Chieftain/Cards.jpg";
+	protected override int ColumnCount => 6;
+	protected override int RowCount => 5;
 }
 
 public abstract class ChieftainCardSide : AbilityCardSide
--- Game/Scripts/Models/FigureTraits/MountTrait.cs
+++ Game/Scripts/Models/FigureTraits/MountTrait.cs
@@ -1,35 +1,130 @@
 using System.Linq;
 using Fractural.Tasks;
+using System;
+using Godot;
 
-public class MountTrait(Figure characterOwner) : FigureTrait
+public class MountTrait() : FigureTrait
 {
+	public class AbstractBuilder<TBuilder, TTrait> :
+		AbstractBuilder<TBuilder, TTrait>.IMountOwnerStep,
+		AbstractBuilder<TBuilder, TTrait>.IOnMountedStep,
+		AbstractBuilder<TBuilder, TTrait>.IOnUnmountedStep
+		where TBuilder : AbstractBuilder<TBuilder, TTrait>
+		where TTrait : MountTrait, new()
+	{
+		protected readonly TTrait Obj = new();
+
+		public interface IMountOwnerStep
+		{
+			TBuilder WithMountOwner(Figure characterOwner);
+		}
+
+		public interface IOnMountedStep
+		{
+			IOnUnmountedStep WithOnMounted(Func<Figure, GDTask> onMounted);
+		}
+
+		public interface IOnUnmountedStep
+		{
+			TBuilder WithOnUnmounted(Func<Figure, GDTask> onUnmounted);
+		}
+
+		public TBuilder WithMountOwner(Figure characterOwner)
+		{
+			Obj._characterOwner = characterOwner;
+			return (TBuilder)this;
+		}
+
+		public IOnUnmountedStep WithOnMounted(Func<Figure, GDTask> onMounted)
+		{
+			Obj._onMounted = onMounted;
+			return (TBuilder)this;
+		}
+
+		public TBuilder WithOnUnmounted(Func<Figure, GDTask> onUnmounted)
+		{
+			Obj._onUnmounted = onUnmounted;
+			return (TBuilder)this;
+		}
+
+		public virtual TTrait Build()
+		{
+			return Obj;
+		}
+	}
+
+	/// <summary>
+	/// A concrete implementation of the AbstractBuilder. Required to actually use the builder,
+	/// as abstract builders cannot be instantiated.
+	/// </summary>
+	public class MountBuilder : AbstractBuilder<MountBuilder, MountTrait>
+	{
+		internal MountBuilder() { }
+	}
+
+	/// <summary>
+	/// A convenience method that returns an instance of SummonBuilder.
+	/// </summary>
+	/// <returns></returns>
+	public static MountBuilder.IMountOwnerStep Builder()
+	{
+		return new MountBuilder();
+	}
+
+	private Figure _characterOwner;
+	private Func<Figure, GDTask> _onMounted;
+	private Func<Figure, GDTask> _onUnmounted;
+
+	private bool _mounted = false;
+
 	public override void Activate(Figure figure)
 	{
 		base.Activate(figure);
 
-        ScenarioCheckEvents.CanEnterHexWithFigureCheckEvent.Subscribe(figure, this,
-			parameters => parameters.OtherFigure == figure && parameters.Figure == characterOwner,
+		// Allow entering the same hex to mount
+		ScenarioCheckEvents.CanEnterHexWithFigureCheckEvent.Subscribe(figure, this,
+			parameters => parameters.OtherFigure == figure && parameters.Figure == _characterOwner,
 			parameters =>
 			{
 				parameters.SetCanEnter();
 			}
 		);
 
-        ScenarioCheckEvents.IsSummonControlledCheckEvent.Subscribe(figure, this,
-			parameters => parameters.Summon == figure && IsMounted(figure, characterOwner),
+		// Control the mount
+		ScenarioCheckEvents.IsSummonControlledCheckEvent.Subscribe(figure, this,
+			parameters => parameters.Summon == figure && _mounted,
 			parameters =>
 			{
 				parameters.SetIsControlled();
 			}
 		);
 
-        ScenarioEvents.MoveTogetherCheckEvent.Subscribe(figure, this,
-			parameters => parameters.Performer == figure && IsMounted(figure, characterOwner),
+		// Follow the mount when it moves or being forcefully moved
+		ScenarioEvents.MoveTogetherCheckEvent.Subscribe(figure, this,
+			parameters => parameters.Performer == figure && _mounted,
 			parameters =>
 			{
-				parameters.SetOtherFigure(characterOwner);
+				parameters.SetOtherFigure(_characterOwner);
 
 				return GDTask.CompletedTask;
+			}
+		);
+
+		// Trigger mounted effect
+		ScenarioEvents.FigureEnteredHexEvent.Subscribe(figure, this,
+			parameters => parameters.Figure == _characterOwner,
+			async parameters =>
+			{
+				if(_onMounted != null && parameters.Hex == figure.Hex)
+				{
+					await _onMounted(_characterOwner);
+					_mounted = true;
+				}
+				else if (_onUnmounted != null && _mounted && parameters.Hex != figure.Hex)
+				{
+					await _onUnmounted(_characterOwner);
+					_mounted = false;
+				}
 			}
 		);
 	}
@@ -39,10 +134,12 @@ public class MountTrait(Figure characterOwner) : FigureTrait
 		base.Deactivate(figure);
 
 		ScenarioCheckEvents.CanEnterHexWithFigureCheckEvent.Unsubscribe(figure, this);
+		ScenarioCheckEvents.IsSummonControlledCheckEvent.Unsubscribe(figure, this);
+		ScenarioEvents.MoveTogetherCheckEvent.Unsubscribe(figure, this);
 	}
 
-    private bool IsMounted(Figure figure, Figure owner)
-    {
-        return figure.Hex.GetHexObjectsOfType<Figure>().Contains(owner);
-    }
+	//private bool IsMounted(Figure figure, Figure owner)
+	//{
+	//    return figure.Hex.GetHexObjectsOfType<Figure>().Contains(owner);
+	//}
 }
